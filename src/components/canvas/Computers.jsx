38313d8d9da16718e53770c8d95132edae37617e
nnawar706 <nnawar706@gmail.com>
2023-08-11 13:09:32 +0600

    pc-config

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { React, Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+  
+  const computer = useGLTF("./desktop_pc/scene.glb");
+  
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene}/>
+    </mesh>
+  )
+}
+
+const ComputerCanvas = () => {
+  return (
+    <Canvas frameloop="demand" shadows 
+    camera={{ position: [20, 3, 5], fov: 25 }}
+    gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader/>}></Suspense>
+    </Canvas>
   )
 }
 
