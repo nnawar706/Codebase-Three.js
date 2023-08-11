9aedd5de3909ac14f1d2d9d69b1005f8fd030708
nnawar706 <nnawar706@gmail.com>
2023-08-11 13:37:10 +0600

    computer placement

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,7 +11,17 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene}/>
+      <spotLight 
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}/>
+      <primitive object={computer.scene} 
+        scale={0.75} 
+        position={[0, -3.25, -2.5]}
+        rotation={[-0.01, -0.2, -0.1]}/>
     </mesh>
   )
 }
@@ -21,9 +31,15 @@ const ComputerCanvas = () => {
     <Canvas frameloop="demand" shadows 
     camera={{ position: [20, 3, 5], fov: 25 }}
     gl={{ preserveDrawingBuffer: true }}>
-      <Suspense fallback={<CanvasLoader/>}></Suspense>
+      <Suspense fallback={<CanvasLoader/>}>
+        <OrbitControls
+          enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}
+        />
+        <Computers/>
+      </Suspense>
+      <Preload all/>
     </Canvas>
   )
 }
 
-export default Computers
+export default ComputerCanvas
