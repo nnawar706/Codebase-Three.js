86d78dc941189a3c2b65aff9a0eaf6b6f04a7f62
nnawar706 <nnawar706@gmail.com>
2023-08-11 14:32:17 +0600

    animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,6 +23,17 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas/>
+      <div className="absolute xs:bottom-20 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary 
+          flex justify-center items-start p-2">
+            <motion.dev 
+              animate={{y:[0,24,0]}} 
+              transition={{}}
+            />
+          </div>
+        </a>
+      </div>
       
     </section>
   )
