c63b971aa71fd418920f1497b069c20ea8425101
nnawar706 <nnawar706@gmail.com>
2023-08-15 20:14:51 +0600

    hero-complete

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = (isMobile) => {
   
   const computer = useGLTF("./desktop_pc/scene.glb");
   
@@ -19,14 +19,36 @@ const Computers = () => {
         castShadow
         shadow-mapSize={1024}/>
       <primitive object={computer.scene} 
-        scale={0.75} 
-        position={[0, -3.25, -2.5]}
+        scale={isMobile? 0.7 : 0.75} 
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -2.5]}
         rotation={[-0.01, -0.2, -0.1]}/>
     </mesh>
   )
 }
 
 const ComputerCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // check if the media size matches 500px and set to state
+    const media = window.matchMedia('(max-width: 500px)'); 
+
+    setIsMobile(media.matches);
+
+    const changeMedia = (event) => {
+      setIsMobile(event.matches);
+    }
+    
+    // change state when media has changed
+    media.addEventListener('change', changeMedia);
+
+    // remove listener when the component is unmounted
+    return () => {
+      media.removeEventListener('change', changeMedia);
+    }
+  }, [])
+
   return (
     <Canvas frameloop="demand" shadows 
     camera={{ position: [20, 3, 5], fov: 25 }}
@@ -35,7 +57,7 @@ const ComputerCanvas = () => {
         <OrbitControls
           enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2}
         />
-        <Computers/>
+        <Computers isMobile={isMobile}/>
       </Suspense>
       <Preload all/>
     </Canvas>
